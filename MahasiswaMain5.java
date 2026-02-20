a53bbb566b8dbf06eb5dd8429041d0e3db40747e
Alvido Wijaya <wijayaalvido@gmail.com>
2026-02-20 14:24:46 +0700

    error lagi

---
+++ MahasiswaMain5.java
@@ -0,0 +1,14 @@
+public class MahasiswaMain5 {
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIPK(3.60);
+        mhs1.tampilkanInformasi();
+    }
+}
