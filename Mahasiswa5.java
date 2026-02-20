24aea513faa11112ae7bedf7c1297a75870a7ceb
Alvido Wijaya <wijayaalvido@gmail.com>
2026-02-20 15:56:12 +0700

    ALHAMDULILLAH BISA

--- Mahasiswa5.java
+++ Mahasiswa5.java
@@ -1,8 +1,19 @@
-public class Mahasiswa5 {
+class Mahasiswa {
+
     String nama;
     String nim;
     String kelas;
     double ipk;
+
+    Mahasiswa() {
+    }
+
+    Mahasiswa(String nm, String nim, double ipk, String kls) {
+        this.nama = nm;
+        this.nim = nim;
+        this.kelas = kls;
+        this.ipk = ipk;
+    }
 
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
@@ -17,21 +28,9 @@ public class Mahasiswa5 {
 
     void updateIPK(double ipkBaru) {
         if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
-        ipk = ipkBaru;
+            ipk = ipkBaru;
         } else {
-        System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
-        }   
-    }
-
-    String nilaiKinerja() {
-        if (ipk >= 3.5) {
-            return "Kinerja sangat baik";
-        } else if (ipk >= 3.0) {
-            return "Kinerja baik";
-        } else if (ipk >= 2.0) {
-            return "Kinerja cukup";
-        } else {
-            return "Kinerja kurang";
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
         }
     }
 }
--- MahasiswaMain5.java
+++ MahasiswaMain5.java
@@ -1,6 +1,6 @@
 public class mahasiswaMain5 {
       public static void main(String[] args) {
-        mahasiswa mhs1 = new mahasiswa();
+        Mahasiswa mhs1 = new Mahasiswa();
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
@@ -10,5 +10,9 @@ public class mahasiswaMain5 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIPK(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa mhs2 = new Mahasiswa("Anissa Nabila" , "2141720160" , 3.25 , "TI 2L");
+        mhs2.updateIPK(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
